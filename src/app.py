2529de311ccc10640fb91f9a8a5c610b8db24ba6
lighteko <dev.lighteko@gmail.com>
2025-02-23 02:19:33 -0500

    INIT: Initial project setting

---
+++ src/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from config import BaseConfig
+
+import logging
+import json
+
+
+def create_app():
+    app = Flask(__name__)
+
+    BaseConfig(app)
+    CORS(app, supports_credentials=True)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    register_blueprints(app)
+
+    @app.route('/health')
+    def health():
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+        return resp
+
+    app.logger.setLevel(logging.DEBUG)
+
+    @app.before_request
+    def log_request_info():
+        app.logger.debug(f"Request Path: {request.path}")
+        app.logger.debug(f"Request Method: {request.method}")
+        app.logger.debug(f"Request Headers: {request.headers}")
+        app.logger.debug(f"Request Data: {request.data}")
+
+    # Define the after_request decorator to log response information
+    @app.after_request
+    def log_response_info(response):
+        # Create a dictionary with the response information
+        response_info = {
+            'Status Code': response.status_code,
+            'Headers': dict(response.headers),
+            'Data': response.data.decode('utf-8')  # Assuming the response data is in UTF-8
+        }
+
+        # Decode the JSON data in the "Data" field
+        try:
+            decoded_data = json.loads(response_info['Data'])
+        except json.JSONDecodeError as e:
+            decoded_data = response_info['Data']
+
+        # Include the decoded data in the log output
+        log_output = f"Decoded Data:\n{decoded_data}"
+        app.logger.debug(log_output)
+
+        return response
+
+    return app
+
+
+def register_blueprints(app: Flask) -> None:
+
+
+    apis = []
+
+    for _api in apis:
+        app.register_blueprint(_api.blueprint)
